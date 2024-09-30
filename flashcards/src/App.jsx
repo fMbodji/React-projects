41405cdd426f5c8602d5ecc0e88a3194e7a0388f
Fatima Mbodji <fmbodji@montgomerycollege.edu>
2024-09-30 18:12:44 -0400

    Adding flashcards  in React-projects

---
+++ flashcards/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import './App.css'
+
+const App = () => {
+
+  return (
+    <div className="App">
+
+      <div className="header">
+        <h1>Welcome to the Flashcards App</h1>
+        <h2>This is a flashcards app that will help you study for your exams</h2>
+        <h3>Number of cards: </h3>
+        <img src="/images/computerBCG-full.jpg" className="computer" alt="Computer Background" />
+      </div>
+
+    </div>
+  )
+}
+
+export default App
